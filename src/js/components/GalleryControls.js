90896342387bc4315e9d13a48e762cf404f8002a
vladem <lairotele@gmail.com>
2020-07-25 18:24:28 +0400

    wrap import forms into modals

--- src/js/components/GalleryControls.js
+++ src/js/components/GalleryControls.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import AddFromJsonForm from './AddFromJsonForm';
 import AddFromLinkForm from './AddFromLinkForm';
+import ButtonModal from './modal/ButtonModal';
+import { ModalContext } from './modal/ModalContext';
+
+const AddFromJsonButton = () => {
+  const { openModal } = useContext(ModalContext);
+
+  return (
+    <button className="button" onClick={openModal}>
+      Импортирвать из JSON
+    </button>
+  );
+};
+
+const AddFromLinkButton = () => {
+  const { openModal } = useContext(ModalContext);
+
+  return (
+    <button className="button" onClick={openModal}>
+      Добавить по ссылке
+    </button>
+  );
+};
 
 const GalleryControls = () => {
   return (
     <div>
       <div>GalleryControls</div>
-      <div className="gallery_controls">
-        <AddFromLinkForm />
-        <AddFromJsonForm />
+      <div className="toolbar toolbar_right">
+        <ButtonModal OpenButton={AddFromLinkButton}>
+          <AddFromLinkForm />
+        </ButtonModal>
+        <ButtonModal OpenButton={AddFromJsonButton}>
+          <AddFromJsonForm />
+        </ButtonModal>
       </div>
     </div>
   );
