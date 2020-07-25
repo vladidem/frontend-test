ba04a7204c0163298206ce8fdeb2c25feff37485
vladem <lairotele@gmail.com>
2020-07-25 11:08:23 +0400

    install react-modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ReactModal from 'react-modal';
+
 import Gallery from './components/Gallery';
 import { GalleryProvider } from './components/GalleryContext';
 
 import '../css/style.scss';
 
 const wrapper = document.getElementById('container');
+
+ReactModal.setAppElement(wrapper);
 
 ReactDOM.render(
   <GalleryProvider>
