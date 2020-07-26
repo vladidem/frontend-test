30b01cf94185e5fa37dbdffae9188b733b08d45a
vladem <lairotele@gmail.com>
2020-07-26 13:23:05 +0400

    add empty gallery placeholder

--- src/js/components/gallery/Gallery.js
+++ src/js/components/gallery/Gallery.js
@@ -3,15 +3,16 @@ import React, { useContext } from 'react';
 import { GalleryContext } from './GalleryContext';
 
 import GalleryItem from './GalleryItem';
+import GalleryPlaceholder from './GalleryPlaceholder';
 
 const Gallery = () => {
   const { images } = useContext(GalleryContext);
 
   return (
     <div className="gallery">
-      {images.map((image) => (
+      {images.length ? images.map((image) => (
         <GalleryItem key={image.id} image={image} />
-      ))}
+      )):<GalleryPlaceholder/>}
     </div>
   );
 };
---
+++ src/js/components/gallery/GalleryPlaceholder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibraryOutlined';
+
+const GalleryPlaceholder = () => {
+  return (
+    <div className="gallery__placeholder">
+      <PhotoLibraryIcon />
+      No photos
+    </div>
+  );
+};
+
+export default GalleryPlaceholder;
