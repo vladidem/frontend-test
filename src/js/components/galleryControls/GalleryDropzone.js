addf20210fb430ecb0ac3d00ed16de6578fbd6e4
vladem <lairotele@gmail.com>
2020-07-27 13:15:08 +0400

    make dropzone take whole screen on file drag

--- src/js/components/galleryControls/GalleryDropzone.js
+++ src/js/components/galleryControls/GalleryDropzone.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 
 import AddToPhotos from '@material-ui/icons/AddToPhotos';
 
@@ -11,6 +11,28 @@ import { GalleryContext } from './../gallery/GalleryContext';
 
 const GalleryDropzone = () => {
   const { addImages } = useContext(GalleryContext);
+
+  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
+  const showOverlay = () => {
+    setIsOverlayVisible(true);
+  };
+  const hideOverlay = () => {
+    setIsOverlayVisible(false);
+  };
+  useEffect(() => {
+    document.addEventListener('dragenter', showOverlay);
+    document.addEventListener('dragover', showOverlay);
+    document.addEventListener('dragleave', hideOverlay);
+    document.addEventListener('drop', hideOverlay);
+    return () => {
+      document.removeEventListener('dragenter', showOverlay);
+      document.removeEventListener('dragover', showOverlay);
+      document.removeEventListener('dragleave', hideOverlay);
+      document.removeEventListener('drop', hideOverlay);
+    }
+  }, []);
+
+
   const onDrop = (acceptedFiles) => {
     addImages(
       acceptedFiles.map((file) => ({
@@ -22,8 +44,13 @@ const GalleryDropzone = () => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   const dropzoneClasses = {
-    gallery__dropzone: true,
-    gallery__dropzone_active: isDragActive,
+    dropzone: true,
+    dropzone_active: isDragActive,
+  };
+
+  const dropzoneOverlayClasses = {
+    dropzone__overlay: true,
+    dropzone__overlay_active: isOverlayVisible,
   };
 
   return (
@@ -33,6 +60,10 @@ const GalleryDropzone = () => {
         <AddToPhotos />
         <p>Выберите или перетащите файлы</p>
       </button>
+      <div className={cx(dropzoneOverlayClasses)}>
+        <AddToPhotos />
+        <p>Перетащите файлы</p>
+      </div>
     </div>
   );
 };
