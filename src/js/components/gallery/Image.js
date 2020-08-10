41e8d335bdc7d2a81ab4223048bebd0a121751c0
vladem <lairotele@gmail.com>
2020-08-10 10:01:56 +0400

    update image dimensions manually, wait for loading manually

--- src/js/components/gallery/Image.js
+++ src/js/components/gallery/Image.js
@@ -1,50 +1,65 @@
-import React, { useRef, Suspense } from 'react';
+import React, { useRef, Suspense, useState, useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
-import { useImage } from 'react-image';
-
 import { ErrorBoundary } from 'react-error-boundary';
+
+import cx from 'classnames';
 
 import Placeholder from './Placeholder';
 import ErrorPlaceholder from './ErrorPlaceholder';
 import { updateImage } from '../../redux/images/actions';
 
-const Image = ({ image }) => {
-  const { url, id, width, height } = image;
-
-  const { src } = useImage({
-    srcList: url,
-  });
-  const ref = useRef();
-  const dispatch = useDispatch();
-
-  const onLoad = () => {
-    if (!width || !height) {
+const pollImageSize = (id, ref, dispatch) => {
+  const poll = setInterval(() => {
+    if (ref.current && ref.current.naturalHeight && ref.current.naturalWidth) {
       const dimensions = {
         width: ref.current.naturalWidth,
         height: ref.current.naturalHeight,
       };
       dispatch(updateImage(id, dimensions));
+      clearInterval(poll);
     }
+  }, 50);
+  return () => clearInterval(poll);
+};
+
+const Image = ({ image }) => {
+  const { url, id, width, height } = image;
+
+  const [loading, setLoading] = useState(true);
+  const ref = useRef();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!width || !height) {
+      return pollImageSize(id, ref, dispatch);
+    }
+  }, [id]);
+
+  const onLoad = () => {
+    setLoading(false);
+  };
+
+  const displayPlaceholder = loading || !width || !height;
+  const classes = {
+    'gallery-item__img': true,
+    'gallery-item__img--hidden': displayPlaceholder,
   };
 
   return (
-    <img
-      className="gallery-item__img"
-      src={src}
-      onLoad={onLoad}
-      ref={ref}
-      loading="lazy"
-    />
+    <>
+      <img
+        className={cx(classes)}
+        src={url}
+        onLoad={onLoad}
+        ref={ref}
+        loading="lazy"
+      />
+      {displayPlaceholder && <Placeholder />}
+    </>
   );
 };
-
-const withSuspense = (Component) => ({ children, ...props }) => (
-  <Suspense fallback={<Placeholder />}>
-    <Component {...props}>{children}</Component>
-  </Suspense>
-);
 
 const withErrorBoundary = (Component) => ({ children, ...props }) => (
   <ErrorBoundary fallback={<ErrorPlaceholder />}>
@@ -52,4 +67,4 @@ const withErrorBoundary = (Component) => ({ children, ...props }) => (
   </ErrorBoundary>
 );
 
-export default withErrorBoundary(withSuspense(Image));
+export default withErrorBoundary(Image);
